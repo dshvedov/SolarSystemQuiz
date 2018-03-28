74b07cd84d22e843e43386ce7d87040fec64fef4
dshvedov <zzz@intome.ru>
2018-03-29 01:41:18 +0300

    reformat code

--- app/src/main/java/ru/d9d/wonderquizz/MainActivity.java
+++ app/src/main/java/ru/d9d/wonderquizz/MainActivity.java
@@ -20,30 +20,31 @@ public class MainActivity extends AppCompatActivity {
 
     public void submit(View view) {
         int correct = 0;
-        EditText answer_star = (EditText) findViewById(R.id.answer_star);
+        EditText answer_star = findViewById(R.id.answer_star);
         if (answer_star.getText().toString().equalsIgnoreCase(getString(R.string.sun))) correct++;
-        EditText answer_dense = (EditText) findViewById(R.id.answer_dense);
-        if (answer_dense.getText().toString().equalsIgnoreCase(getString(R.string.earth))) correct++;
+        EditText answer_dense = findViewById(R.id.answer_dense);
+        if (answer_dense.getText().toString().equalsIgnoreCase(getString(R.string.earth)))
+            correct++;
 
-        RadioButton radio_answer_mercury = (RadioButton) findViewById(R.id.radio_answer_mercury);
+        RadioButton radio_answer_mercury = findViewById(R.id.radio_answer_mercury);
         if (radio_answer_mercury.isChecked()) correct++;
 
-        RadioButton radio_answer_venus = (RadioButton) findViewById(R.id.radio_answer_venus);
+        RadioButton radio_answer_venus = findViewById(R.id.radio_answer_venus);
         if (radio_answer_venus.isChecked()) correct++;
 
-        RadioButton radio_answer_hot = (RadioButton) findViewById(R.id.radio_answer_hot);
+        RadioButton radio_answer_hot = findViewById(R.id.radio_answer_hot);
         if (radio_answer_hot.isChecked()) correct++;
 
-        RadioButton radio_answer_jupiter = (RadioButton) findViewById(R.id.radio_answer_jupiter);
+        RadioButton radio_answer_jupiter = findViewById(R.id.radio_answer_jupiter);
         if (radio_answer_jupiter.isChecked()) correct++;
 
-        RadioButton radio_answer_ancients = (RadioButton) findViewById(R.id.radio_answer_ancients);
+        RadioButton radio_answer_ancients = findViewById(R.id.radio_answer_ancients);
         if (radio_answer_ancients.isChecked()) correct++;
 
-        CheckBox answer_rings_jupiter = (CheckBox) findViewById(R.id.answer_rings_jupiter);
-        CheckBox answer_rings_saturn = (CheckBox) findViewById(R.id.answer_rings_saturn);
-        CheckBox answer_rings_uranus = (CheckBox) findViewById(R.id.answer_rings_uranus);
-        CheckBox answer_rings_neptune = (CheckBox) findViewById(R.id.answer_rings_neptune);
+        CheckBox answer_rings_jupiter = findViewById(R.id.answer_rings_jupiter);
+        CheckBox answer_rings_saturn = findViewById(R.id.answer_rings_saturn);
+        CheckBox answer_rings_uranus = findViewById(R.id.answer_rings_uranus);
+        CheckBox answer_rings_neptune = findViewById(R.id.answer_rings_neptune);
         if (answer_rings_jupiter.isChecked() &&
                 answer_rings_saturn.isChecked() &&
                 answer_rings_uranus.isChecked() &&
